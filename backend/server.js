ea4fe7ca13c56abecb823710a0a8d1a3c98e7519
cjPratham <pratham007yadav@gmail.com>
2025-08-09 12:02:16 +0530

    removed mongo URI

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // MongoDB connection
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log(`MongoDB connected: ${process.env.MONGO_URI}`))
+  .then(() => console.log(`MongoDB connected`))
   .catch(err => console.error(err));
 
 
